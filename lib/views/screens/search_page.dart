f8d4be2ca8a5ecf5076910375b897612db3b3a6c
Almonzer Osman <almonzer.m.osman@gmail.com>
2024-04-19 11:47:41 +0400

    Adding logic for searching a booking reference
    - If no results found
    - A card to display the result info
    - An image with no resluts found text if the search was not successful
    
    Signed-off-by: Almonzer Osman <almonzer.m.osman@gmail.com>

--- lib/views/screens/search_page.dart
+++ lib/views/screens/search_page.dart
@@ -1,3 +1,5 @@
+import 'package:checkedin/views/widgets/search_result.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:checkedin/models/core/recipe.dart';
@@ -14,130 +16,197 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   TextEditingController searchInputController = TextEditingController();
   final List<Recipe> searchResult = RecipeHelper.sarchResultRecipe;
+  var searchedBooking = [];
+  var searchNotFound = false;
 
   @override
   Widget build(BuildContext context) {
-    void submit(value) {
-      print(value);
-    }
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('bookings').snapshots(),
+      builder: (ctx, snapshot) {
+        void submit(referenceNumber) {
+          final referenceList = snapshot.data!.docs;
+          searchedBooking.clear();
+          searchNotFound = false;
+          for (var element in referenceList) {
+            if (element.id == referenceNumber.toString().toUpperCase()) {
+              setState(() {
+                searchedBooking.add(element);
+              });
+            }
+            if (searchedBooking.isEmpty) {
+              setState(() {
+                searchNotFound = true;
+              });
+            }
+          }
+        }
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.primary,
-        elevation: 0,
-        centerTitle: true,
-        title: Text(
-          'Search your flight',
-          style: TextStyle(
-            fontFamily: 'inter',
-            fontWeight: FontWeight.w400,
-            fontSize: 16,
-            color: AppColor.whiteSoft,
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: AppColor.primary,
+            elevation: 0,
+            centerTitle: true,
+            title: Text(
+              'Search your flight',
+              style: TextStyle(
+                fontFamily: 'inter',
+                fontWeight: FontWeight.w400,
+                fontSize: 16,
+                color: AppColor.whiteSoft,
+              ),
+            ),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
           ),
-        ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
-      body: ListView(
-        shrinkWrap: true,
-        physics: const BouncingScrollPhysics(),
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            color: AppColor.primary,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Search Bar
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Search TextField
-                      Expanded(
-                        child: Container(
-                          height: 50,
-                          margin: const EdgeInsets.only(right: 15),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: AppColor.primarySoft),
-                          child: TextField(
-                            controller: searchInputController,
-                            onSubmitted: submit,
-                            // onChanged: (value) {
-                            //   print(searchInputController.text);
-                            //   setState(() {});
-                            // },
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400),
-                            maxLines: 1,
-                            textInputAction: TextInputAction.search,
-                            textCapitalization: TextCapitalization.characters,
-                            decoration: InputDecoration(
-                              hintText: 'Enter your booking reference',
-                              hintStyle: TextStyle(
-                                  color: Colors.white.withOpacity(0.2)),
-                              prefixIconConstraints:
-                                  const BoxConstraints(maxHeight: 20),
-                              contentPadding:
-                                  const EdgeInsets.symmetric(horizontal: 17),
-                              focusedBorder: InputBorder.none,
-                              border: InputBorder.none,
-                              prefixIcon: Visibility(
-                                visible: (searchInputController.text.isEmpty)
-                                    ? true
-                                    : false,
-                                child: Container(
-                                  margin: const EdgeInsets.only(
-                                      left: 10, right: 12),
-                                  child: SvgPicture.asset(
-                                    'assets/icons/search.svg',
-                                    width: 20,
-                                    height: 20,
+          body: ListView(
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(),
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                color: AppColor.primary,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Search Bar
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // Search TextField
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              margin: const EdgeInsets.only(right: 15),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: AppColor.primarySoft),
+                              child: TextField(
+                                controller: searchInputController,
+                                onSubmitted: submit,
+                                style: const TextStyle(
                                     color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400),
+                                maxLines: 1,
+                                textInputAction: TextInputAction.search,
+                                textCapitalization:
+                                    TextCapitalization.characters,
+                                decoration: InputDecoration(
+                                  hintText: 'Enter your booking reference',
+                                  hintStyle: TextStyle(
+                                      color: Colors.white.withOpacity(0.2)),
+                                  prefixIconConstraints:
+                                      const BoxConstraints(maxHeight: 20),
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      horizontal: 17),
+                                  focusedBorder: InputBorder.none,
+                                  border: InputBorder.none,
+                                  prefixIcon: Visibility(
+                                    visible:
+                                        (searchInputController.text.isEmpty)
+                                            ? true
+                                            : false,
+                                    child: Container(
+                                      margin: const EdgeInsets.only(
+                                          left: 10, right: 12),
+                                      child: SvgPicture.asset(
+                                        'assets/icons/search.svg',
+                                        width: 20,
+                                        height: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+              if (searchedBooking.isEmpty && searchNotFound == false)
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  width: 400,
+                  height: 400,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/search-image.png'),
+                    ),
+                  ),
+                  child: const Text(
+                    'Start searching your flight',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'inter',
+                    ),
                   ),
                 ),
-              ],
-            ),
+              if (searchedBooking.isEmpty && searchNotFound == true)
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  width: 400,
+                  height: 400,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/no-results.png'),
+                    ),
+                  ),
+                  child: const Text(
+                    'No results found',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'inter',
+                    ),
+                  ),
+                ),
+              if (searchedBooking.isNotEmpty)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: SearchResult(
+                    airline: searchedBooking[0]['airline'],
+                    airlineLogo: searchedBooking[0]['airline_logo'],
+                    arrivalTimestamp: searchedBooking[0]['arrival_timestamp'],
+                    departureAirportCode: searchedBooking[0]
+                        ['departure_airport_code'],
+                    departureAirportName: searchedBooking[0]
+                        ['departure_airport_name'],
+                    departureCity: searchedBooking[0]['departure_city'],
+                    departureCountry: searchedBooking[0]['departure_country'],
+                    departureTimestamp: searchedBooking[0]
+                        ['departure_timestamp'],
+                    destinationAirportCode: searchedBooking[0]
+                        ['destination_airport_code'],
+                    destinationAirportName: searchedBooking[0]
+                        ['destination_airport_name'],
+                    destinationCity: searchedBooking[0]['destination_city'],
+                    destinationCountry: searchedBooking[0]
+                        ['destination_country'],
+                    flightDurationTime: searchedBooking[0]
+                        ['flight_duration_time'],
+                    name: searchedBooking[0]['name'],
+                  ),
+                ),
+            ],
           ),
-          Container(
-            alignment: Alignment.bottomCenter,
-            width: 400,
-            height: 400,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/search-image.png'),
-              ),
-            ),
-            child: const Text(
-              'Start searching your flight',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                fontFamily: 'inter',
-              ),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
---
+++ lib/views/widgets/search_result.dart
@@ -0,0 +1,164 @@
+import 'package:checkedin/views/utils/AppColor.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class SearchResult extends StatelessWidget {
+  final String airline;
+  final String airlineLogo;
+  final Timestamp arrivalTimestamp;
+  final String departureAirportCode;
+  final String departureAirportName;
+  final String departureCity;
+  final String departureCountry;
+  final Timestamp departureTimestamp;
+  final String destinationAirportCode;
+  final String destinationAirportName;
+  final String destinationCity;
+  final String destinationCountry;
+  final String flightDurationTime;
+  final String name;
+
+  const SearchResult({
+    super.key,
+    required this.airline,
+    required this.airlineLogo,
+    required this.arrivalTimestamp,
+    required this.departureAirportCode,
+    required this.departureAirportName,
+    required this.departureCity,
+    required this.departureCountry,
+    required this.departureTimestamp,
+    required this.destinationAirportCode,
+    required this.destinationAirportName,
+    required this.destinationCity,
+    required this.destinationCountry,
+    required this.flightDurationTime,
+    required this.name,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Display the airline name and logo
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                airline,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Image.network(
+                airlineLogo,
+                width: 50,
+                height: 50,
+              ),
+            ],
+          ),
+          // Display the departure and arrival information
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Departure',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '$departureAirportName ($departureAirportCode) - $departureCity, $departureCountry',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                'Departure Time: ${departureTimestamp.toDate()}',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Arrival',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '$destinationAirportName ($destinationAirportCode) - $destinationCity, $destinationCountry',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                'Arrival Time: ${arrivalTimestamp.toDate()}',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          // Display the flight duration
+          Text(
+            'Flight Duration: $flightDurationTime minutes',
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // Display the passenger name
+          Text(
+            'Passenger: $name',
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // Display the confirmation button
+          Container(
+            margin: const EdgeInsets.only(top: 32, bottom: 6),
+            width: MediaQuery.of(context).size.width,
+            height: 60,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                backgroundColor: AppColor.primarySoft,
+              ),
+              child: Text(
+                'Cofrim Flight',
+                style: TextStyle(
+                    color: AppColor.secondary,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'inter'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    ;
+  }
+}
